9ce16a93c693cae387e49c05c8d95cf98e7fad79
abdoul-rb <abdoulrahimbah6@gmail.com>
2024-01-28 21:28:41 +0100

    Update create event

--- app/Http/Controllers/Dashboard/EventController.php
+++ app/Http/Controllers/Dashboard/EventController.php
@@ -25,7 +25,9 @@ class EventController extends Controller
      */
     public function create()
     {
-        return view('dashboard.events.create');
+        $event = new Event();
+
+        return view('dashboard.events.create', compact('event'));
     }
 
     /**
